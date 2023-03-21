30a7db213423f53478ddff9f2af29e1e54d04ac4
OlgaKryushycheva <115803033+OlgaKryushycheva@users.noreply.github.com>
2023-03-21 11:13:59 +0000

    Deploying to gh-pages from @ OlgaKryushycheva/goit-js-hw-10@50a10b96a98cb06a9a74ae9ba7c344bba3638f30 🚀

---
+++ 01-color-switcher.8e268855.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start"),e=document.querySelector("button[data-stop");e.setAttribute("disabled","");var r=null;t.addEventListener("click",(function(){r=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.setAttribute("disabled",""),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled"),e.setAttribute("disabled","")}))}();
+//# sourceMappingURL=01-color-switcher.8e268855.js.map
