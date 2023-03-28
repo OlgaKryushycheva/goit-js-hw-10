40eadcb985c9b4e1db4756fbb6035defee92c88e
OlgaKryushycheva <115803033+OlgaKryushycheva@users.noreply.github.com>
2023-03-28 12:48:01 +0300

    тест

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,41 @@
-const BASE_URL = '';
+import Notiflix from 'notiflix';
 
-export function fetchCountries(name) {
-  console.log(name);
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+// const PARAMS = 'fields=${countryName},capital,population,languages,flag';
+
+export function fetchCountries(countryName) {
+  fetch(
+    `${BASE_URL}${countryName}?fields=name,capital,population,languages,flags&&fullText=false`
+  ).then(response => {
+    if (!response.ok) {
+      // throw new Error(response.status);
+      throw Notiflix.Notify.failure('Oops, there is no country with that name');
+    }
+    console.log(response.json());
+    return response.json();
+  });
+  // .then(data => {
+  //   if (data.length > 10) {
+  //     throw Notiflix.Notify.info(
+  //       `Too many matches found. Please enter a more specific name.`
+  //     );
+  //   }
+  //   console.log(data);
+  //   // console.log(...data);
+  //   const dataObj = data[0];
+  //   console.log(dataObj);
+  //   console.log(dataObj.name.official);
+  //   console.log(dataObj.flags.svg);
+  //   console.log(...dataObj.capital);
+  //   console.log(Object.values(dataObj.languages).join(', '));
+  //   console.log(dataObj.population);
+  // })
+  // .catch(error => {
+  //   console.log(error);
+  // });
 }
+
+// https://restcountries.com/v3.1/all?fields=name,capital,currencies
 
 //https: restcountries.eu/rest/v2/all?fields=name;capital;currencies
 // https://restcountries.eu/rest/v2/{service}?fields={field};{field};{field}
@@ -11,3 +44,33 @@ export function fetchCountries(name) {
 // population;
 // languages;
 // flag;
+
+// const searchParams = new URLSearchParams({
+//   _limit: 10,
+//   // _sort: 'name',
+//   _name: 'name.official',
+//   _capital: '',
+//   _population: 0,
+//   _languages: [],
+//   _flag: 'flags.svg',
+// });
+
+// console.log(searchParams.toString()); // "_limit=5&_sort=name"
+
+// const url = `https://jsonplaceholder.typicode.com/users?${searchParams}`;
+// console.log(url); // "https://jsonplaceholder.typicode.com/users?_limit=5&_sort=name"
+
+// fetch('https://jsonplaceholder.typicode.com/users')
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+// throw Notiflix.Notify.failure('Oops, there is no country with that name');
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     // Data handling
+//   })
+//   .catch(error => {
+//     // Error handling
+//   });
--- src/index.js
+++ src/index.js
@@ -2,14 +2,104 @@ import debounce from 'lodash.debounce';
 // player.on('timeupdate', debounce(getCurrentTime, DEBOUNCE_DELAY));
 
 import Notiflix from 'notiflix';
-Notiflix.Notify.info(
-  `Too many matches found. Please enter a more specific name.`
-);
-Notiflix.Notify.failure('Oops, there is no country with that name');
+// Notiflix.Notify.info(
+//   `Too many matches found. Please enter a more specific name.`
+// );
+// Notiflix.Notify.failure('Oops, there is no country with that name');
 
 import './css/styles.css';
 
 import { fetchCountries } from './fetchCountries';
-fetchCountries('Ukraine');
+// fetchCountries('Ukraine');
 
 const DEBOUNCE_DELAY = 300;
+
+const searchInputEl = document.querySelector('#search-box');
+const countryListEl = document.querySelector('.country-list');
+const countryInfotEl = document.querySelector('.country-info');
+
+searchInputEl.addEventListener(
+  'input',
+  debounce(onSearchCountry, DEBOUNCE_DELAY)
+);
+function onSearchCountry() {
+  const searchQuery = searchInputEl.value.trim();
+  console.log(searchQuery);
+  fetchCountries(searchQuery).then(console.log);
+}
+
+//  .then(data => {
+//       if (data.length > 10) {
+//         throw Notiflix.Notify.info(
+//           `Too many matches found. Please enter a more specific name.`
+//         );
+//       }
+//       console.log(data);
+//       // console.log(...data);
+//       const dataObj = data[0];
+//       console.log(dataObj);
+//       console.log(dataObj.name.official);
+//       console.log(dataObj.flags.svg);
+//       console.log(...dataObj.capital);
+//       console.log(Object.values(dataObj.languages).join(', '));
+//       console.log(dataObj.population);
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+
+const makeCountryList = ({ flag, name }) => `<li class="js-gallery-item">
+        <img class="js-img"src="${url}" alt="${alt}" />
+      </li>`;
+
+// const markupCountryList = images.map(el => makeCountryList(el)).join('');
+// countryListEl.insertAdjacentHTML('afterbegin', markupCountryList);
+
+const makeCountryInfo = ({
+  flag,
+  name,
+  papital,
+  population,
+  languages,
+}) => `<li class="js-gallery-item">
+        <img class="js-img"src="${url}" alt="${alt}" />
+      </li>`;
+
+// const markupCountryInfo = images.map(el => makeCountryList(el)).join('');
+// countryInfotEl.insertAdjacentHTML('afterbegin', markupCountryInfo);
+
+// ==================================================================
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
+
+// fetch(`${BASE_URL}?fields=Ukraine,capital,population,languages,flags`);
+// https://restcountries.com/v3.1/name/aruba?fullText=true
+// fetch(`${BASE_URL}/sud?fullText=false`);
+// fetch(`${BASE_URL}sudan?fields=name,capital,population,languages,flags`);
+
+// fetch(
+//   `${BASE_URL}sud?fields=name,capital,population,languages,flags&&fullText=false`
+// )
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error(
+//         Notiflix.Notify.failure('Oops, there is no country with that name')
+//       );
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//     console.log(...data);
+//     const dataObj = data[0];
+//     console.log(dataObj);
+//     console.log(dataObj.name.official);
+//     console.log(dataObj.flags.svg);
+//     console.log(...dataObj.capital);
+//     console.log(Object.values(dataObj.languages).join(', '));
+//     console.log(dataObj.population);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+console.log('TEST');
