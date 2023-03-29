a514c3430b25d2b314c238656ce9ae6adbd8d095
OlgaKryushycheva <115803033+OlgaKryushycheva@users.noreply.github.com>
2023-03-29 12:51:59 +0300

    ДЗ к сдаче

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,110 +1,13 @@
 import Notiflix from 'notiflix';
 
-const searchInputEl = document.querySelector('#search-box');
-const countryListEl = document.querySelector('.country-list');
-const countryInfoEl = document.querySelector('.country-info');
-
 const BASE_URL = 'https://restcountries.com/v3.1/name/';
+const FIELDS = 'fields=name,capital,population,languages,flags';
 
 export function fetchCountries(countryName) {
-  fetch(
-    `${BASE_URL}${countryName}?fields=name,capital,population,languages,flags&&fullText=false`
-  )
-    .then(response => {
-      if (!response.ok) {
-        throw Notiflix.Notify.failure(
-          'Oops, there is no country with that name'
-        );
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (data.length > 10) {
-        throw Notiflix.Notify.info(
-          `Too many matches found. Please enter a more specific name.`
-        );
-      } else if (data.length > 1 && data.length <= 10) {
-        countryInfoEl.innerHTML = '';
-
-        let countryCollectionArr = [];
-        let countryCollection;
-
-        for (let i = 0; i < data.length; i++) {
-          let name = data[i].name.official;
-          let flag = data[i].flags.svg;
-
-          const markupCountryList = ` <li class="country-item">
-          <img
-            src="${flag}"
-            alt="flag"
-            width="25"
-            height="20"
-          />
-          <p class="country-name">${name}</p>
-        </li>`;
-
-          countryCollectionArr.push(markupCountryList);
-          countryCollection = countryCollectionArr.join('');
-        }
-
-        countryListEl.innerHTML = countryCollection;
-      } else {
-        countryListEl.innerHTML = '';
-
-        let dataSity = data[0];
-        let name = dataSity.name.official;
-        let flag = dataSity.flags.svg;
-        let capital = dataSity.capital[0];
-        let languages = Object.values(dataSity.languages).join(', ');
-        let population = dataSity.population;
-
-        const markupCountryInfo = `<div class="country-title">
-          <img
-            src="${flag}"
-            alt="flag"
-            width="30"
-            height="25"
-          />
-          <p>${name}</p>
-        </div>
-        <p class="country-descr">
-          <span class="country-field">Capital: </span>${capital}
-        </p>
-        <p class="country-descr">
-          <span class="country-field">Population: </span>${population}
-        </p>
-        <p class="country-descr">
-          <span class="country-field">Languages: </span>${languages}
-        </p>`;
-        countryInfoEl.innerHTML = markupCountryInfo;
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  return fetch(`${BASE_URL}${countryName}?${FIELDS}`).then(response => {
+    if (!response.ok) {
+      throw Notiflix.Notify.failure('Oops, there is no country with that name');
+    }
+    return response.json();
+  });
 }
-
-// =============================
-// function makeCountryInfo({ flag, name, capital, population, languages }) {
-//   ` <div class="country-title">
-//         <img
-//           src="${flag}"
-//           alt="flag"
-//           width="25"
-//           height="20"
-//         />
-//         <p>${name}</p>
-//       </div>
-
-//       <p class="country-descr">
-//         <span class="country-field">Capital: </span>${capital}
-//       </p>
-//       <p class="country-descr">
-//         <span class="country-field">Population: </span>${population}
-//       </p>
-//       <p class="country-descr">
-//         <span class="country-field">Languages: </span>${languages}
-//       </p>`;
-
-//   // countryInfoEl.innerHTML = markupCountryInfo;
-// }
